ee7a4b43b0843d277d185288e5bc131e99cdc847
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-02 01:33:13 -0300

    Added Header component and Vuex store to Vue

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import store from './vuex/store';
+import Header from './components/globals/Header';
 
 Vue.config.productionTip = false;
-
+Vue.component('Header', Header);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>',
 });
