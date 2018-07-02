fb0acf4adaecbcbc2a1c8a6b40067c0ed7f538d5
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-02 03:59:29 -0300

    Styled UserSearch

--- src/components/user/UserSearch.vue
+++ src/components/user/UserSearch.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <Header />
-    <form onsubmit="return false" class="search__form">
-      <input type="search__input" class="input-search"
-      @keyup.enter="redirectToUser($event.target.value)"
-      @input="debounceInput">
-      <button @onClick="redirectToUser()" class="search__button">Buscar</button>
-    </form>
-    {{this.$store.state.user.login}}
+    <div class="container">
+      <form onsubmit="return false" class="search__form">
+        <div class="input-group mb-3">
+          <input type="search" class="search__input form-control"
+          @keyup.enter="redirectToUser($event.target.value)"
+          @input="debounceInput" placeholder="Faça sua busca aqui...">
+          <div class="input-group-append">
+            <button @onClick="redirectToUser()"
+            class="search__button btn btn-primary">Buscar</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 <script>
@@ -27,9 +33,11 @@ export default {
       1000),
 
     redirectToUser(value) {
-      this.$store.dispatch('fetchUser', value).then(() => {
-        this.$router.push(`/user/${this.$store.state.user.login}`);
-      });
+      if (value.length) {
+        this.$store.dispatch('fetchUser', value).then(() => {
+          this.$router.push(`/user/${this.$store.state.user.login}`);
+        });
+      }
     },
   },
 };
