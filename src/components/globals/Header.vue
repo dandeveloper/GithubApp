f5e14fc6a74eeaf4f907f4256f3ab4e5d32e4785
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-02 01:20:03 -0300

    Created Header simple base

---
+++ src/components/globals/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="main-header">
+    <h1>Github APP</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style lang="postcss" scoped>
+
+</style>
