a29c108293864118c2d894de517b37e247314e16
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-03 02:29:52 -0300

    Created FETCH_USER_REPOS, FETCH_REPO and REQUEST_ERROR mutations

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -4,4 +4,20 @@ export default {
       state.user = payload;
     }
   },
+  FETCH_USER_REPOS(state, payload) {
+    if (state.user.login) {
+      payload.forEach((element) => {
+        state.repos.push(element);
+      });
+    }
+  },
+  FETCH_REPO(state, payload) {
+    state.repo = payload;
+  },
+  REQUEST_ERROR(state, payload) {
+    if (payload !== null) {
+      state.error = payload;
+      console.log('Erro no servidor.', payload);
+    }
+  },
 };
