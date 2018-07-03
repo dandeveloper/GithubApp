a4b119947507aed79e0cb0452c469ab484a0f047
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-03 02:35:26 -0300

    Some linter fixes

--- src/components/user/UserReposList.vue
+++ src/components/user/UserReposList.vue
@@ -2,7 +2,9 @@
   <div class="user__repos">
     <ul class="user__repos-list">
       <li v-for="repo in reposList" :key="repo.id">
-          <a href="#" class="repo__link" @onClick="goToRepo(repo.full_name, repo.name)" >{{ repo.name }}</a>
+          <a href="#" class="repo__link" @onClick="goToRepo(repo.full_name, repo.name)" >
+            {{ repo.name }}
+          </a>
       </li>
     </ul>
   </div>
@@ -18,10 +20,10 @@ export default {
     this.$store.dispatch('fetchUserRepos', this.$store.state.user.login);
   },
   methods: {
-    redirectToRepo(full_name, name) {
-      this.dispatch('fetchRepo', full_name);
+    redirectToRepo(fullName, name) {
+      this.dispatch('fetchRepo', fullName);
       this.$router.push(`/repo/${name}`);
-    }
-  }
+    },
+  },
 };
 </script>
---
+++ test/unit/specs/components/user/UserDetails.spec.js
@@ -0,0 +1,19 @@
+// import Vue from 'vue';
+// import UserDetails from '@/components/user/UserDetails';
+
+// describe('UserDetails.vue', () => {
+//   it('should render correct contents', () => {
+//     const Constructor = Vue.extend(UserDetails);
+//     const vm = new Constructor().$mount();
+//     expect(vm.$el.querySelector('.hello h1').textContent)
+//       .toEqual('Welcome to Your Vue.js App');
+//   });
+// });
+// // describe('HelloWorld.vue', () => {
+// //   it('should render correct contents', () => {
+// //     const Constructor = Vue.extend(HelloWorld);
+// //     const vm = new Constructor().$mount();
+// //     expect(vm.$el.querySelector('.hello h1').textContent)
+// //       .toEqual('Welcome to Your Vue.js App');
+// //   });
+// // });
