260c7ff6ceff562869ea8cd5a57512b899389863
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-03 02:32:31 -0300

    Created base component User

---
+++ src/components/user/User.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="user">
+    <Header />
+    <userDetails />
+    <userRepos />
+  </section>
+</template>
+<script>
+import UserDetails from '@/components/user/UserDetails';
+import UserReposList from '@/components/user/UserReposList';
+
+export default {
+  mounted() {
+    this.checkInvalidUser();
+  },
+  methods: {
+    checkInvalidUser() {
+      if (!this.$store.state.user.login.length) {
+        this.$router.push('/');
+      }
+    },
+  },
+  components: {
+    userDetails: UserDetails,
+    userRepos: UserReposList,
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
