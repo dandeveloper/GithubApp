837f7c44c66c746d1d33ca1c964e137b59a7643d
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-04 11:11:28 -0300

    Added Repo Component as Modal

--- src/components/repo/Repo.vue
+++ src/components/repo/Repo.vue
@@ -1,43 +1,81 @@
 <template>
-  <section class="repo">
-    <Header />
-    <div class="container">
-      <div class="row">
-        <div class="repo__header">
-          <h2 class="repo__name">
-            {{this.name}}
-          </h2>
-          <span class="stars">Estrelas {{this.stars}}</span>
-          <span class="language">{{this.language}}</span>
-          <a :href="`https://github.com/${this.full_name}`" class="repo__link" target="_blank">
-            {{`https://github.com/${this.full_name}`}}
-          </a>
-        </div>
-        <div class="repo__body">
-          <div class="repo__description">
-            {{this.description}}
-          </div>
-        </div>
-      </div>
+  <b-modal ref="repoModal" size="lg"
+  hide-footer cancel-disabled no-close-on-esc hide-header-close no-close-on-backdrop
+  :title="repo.name">
+    <div class="d-block">
+      <span class="stars"><i class="fa fa-star"></i> Estrelas {{repo.stargazers_count}}</span>
+      <span class="language">{{repo.language}}</span>
+      <a :href="`https://github.com/${repo.full_name}`" class="repo__link" target="_blank">
+        <i class="fa fa-link"></i> URL: {{`https://github.com/${repo.full_name}`}}
+      </a>
     </div>
-  </section>
+    <h3 class="repo__full-name d-block">{{repo.full_name}}</h3>
+    <div class="d-block repo__description-wrapper">
+      <p class="repo__description">{{repo.description}}</p>
+    </div>
+    <b-btn class="mt-3" variant="success" block @click="hideModal">Fechar</b-btn>
+  </b-modal>
 </template>
 <script>
 export default {
   data() {
     return {
-      repo: {},
+      repo: this.$store.state.repo,
     };
   },
+  created() {
+    this.$store.watch(
+      (state) => {
+        this.repo = this.$store.state.repo;
+        if (state.showRepoDetails === true) {
+          this.$refs.repoModal.show();
+        }
+      },
+    );
+  },
   mounted() {
-    this.getRepoState();
+    this.repo = this.$store.state.repo;
   },
   methods: {
-    getRepoState() {
-      if (typeof this.$store.state.repo !== 'undefined') {
-        this.repo = this.$store.state.repo;
-      }
+    showModal() {
+      this.$refs.repoModal.show();
+    },
+    hideModal() {
+      this.$store.dispatch('showRepoDetails', false);
+      this.$refs.repoModal.hide();
     },
   },
 };
 </script>
+<style lang="postcss" scoped>
+.stars {
+  background: #eaeaea;
+  border-radius: 3px;
+  padding: 3px 5px;
+  font-size: 14px;
+}
+.language {
+    background: #eaeaea;
+    border-radius: 3px;
+    padding: 3px 5px;
+    font-size: 14px;
+    font-weight: 600;
+    margin: 0 5px;
+}
+.d-block {
+  padding: 10px 20px;
+}
+@media screen and (max-width: 768px) {
+  .d-block {
+    padding: 10px 0px;
+  }
+  .repo__link {
+    display: block;
+    margin-top: 10px;
+    font-size: 14px
+  }
+  .repo__full-name {
+    font-size: 1.5rem;
+  }
+}
+</style>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,12 +3,14 @@
     <Header />
     <userDetails />
     <userReposList />
+    <repo />
     <errorModal />
   </section>
 </template>
 <script>
 import UserDetails from '@/components/user/UserDetails';
 import UserReposList from '@/components/user/UserReposList';
+import Repo from '@/components/repo/Repo';
 import ErrorModal from '@/components/globals/ErrorModal';
 
 export default {
@@ -26,6 +28,7 @@ export default {
     userDetails: UserDetails,
     userReposList: UserReposList,
     errorModal: ErrorModal,
+    repo: Repo,
   },
 };
 </script>
