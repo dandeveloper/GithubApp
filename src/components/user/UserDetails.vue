8ba9cf20e538907e4d0727e5380e9bfc14a5bad1
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-04 11:11:50 -0300

    Fixing and styling UserDetails component

--- src/components/user/UserDetails.vue
+++ src/components/user/UserDetails.vue
@@ -2,21 +2,21 @@
   <div class="user__details container">
     <div class="row">
       <div class="col-md-4">
-        <img :src="this.user.avatar_url" class="user__avatar">
+        <img :src="user.avatar_url" class="user__avatar">
       </div>
       <div class="user__infos col-md-8">
         <div class="social">
           <span class="seguidores">
-            Seguidores <span class="quantity">{{this.user.followers}}</span>
+            Seguidores <span class="badge badge-primary badge-pill">{{user.followers}}</span>
           </span>
           <span class="seguidores">
-            Seguindo <span class="quantity">{{this.user.following}}</span>
+            Seguindo <span class="badge badge-primary badge-pill">{{user.following}}</span>
           </span>
         </div>
-        <h2 class="user__name">{{this.user.name}}</h2>
-        <strong class="user__login">{{this.user.login}}</strong>
-        <a :href="`mailto:${this.user.email}`" class="user__email">{{this.user.email}}</a>
-        <p class="user__bio">{{this.user.bio}}</p>
+        <h2 class="user__name">{{user.name}}</h2>
+        <strong class="user__login">{{user.login}}</strong>
+        <a :href="`mailto:${user.email}`" class="user__email">{{user.email}}</a>
+        <p class="user__bio">{{user.bio}}</p>
       </div>
     </div>
   </div>
@@ -26,13 +26,12 @@
 export default {
   data() {
     return {
-      user: {},
+      user: this.$store.state.user,
     };
   },
   mounted() {
     this.getUserState();
   },
-
   methods: {
     getUserState() {
       if (this.$store.state.user.login.length) {
@@ -52,14 +51,16 @@ export default {
 .seguidores, .seguidores {
   font-size: 14px;
 }
-.quantity {
-  border-radius: 100%;
-  background: rgba(27,31,35,0.08);
-  padding: 2px 5px;
-  text-align: center;
+.seguidores {
   margin-right: 20px;
 }
 .user__email {
   display: block;
+}
+.user__details {
+    padding: 40px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    margin-bottom: 5vh;
 }
 </style>
