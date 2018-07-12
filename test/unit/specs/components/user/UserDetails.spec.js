bb7f23a19d5eeb79f7ec160ec211df993753ab40
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-11 23:40:52 -0300

    Refatoring UserDetails tests

--- test/unit/specs/components/user/UserDetails.spec.js
+++ test/unit/specs/components/user/UserDetails.spec.js
@@ -1,23 +1,14 @@
 import Vuex from 'vuex';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import UserDetails from '@/components/user/UserDetails';
-import state from '@/vuex/state';
 
 const localVue = createLocalVue();
+
 localVue.use(Vuex);
 
 describe('UserDetails', () => {
-  let store;
-  let actions;
   let wrapper;
   beforeEach(() => {
-    actions = {
-      fetchUser: jest.fn(),
-    };
-    store = new Vuex.Store({
-      actions,
-      state,
-    });
     wrapper = shallowMount(UserDetails, {
       mocks: {
         $store: {
@@ -34,51 +25,28 @@ describe('UserDetails', () => {
           },
         },
       },
-      store,
       localVue,
     });
   });
   it('expects user has a login value', () => {
-    function callback() {
-      expect(wrapper.vm.user.login).toBe('loginTeste');
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.login).toBe('loginTeste');
   });
   it('expects user has a name value', () => {
-    function callback() {
-      expect(wrapper.vm.user.name).toBe('name test');
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.name).toBe('name test');
   });
   it('expects user has a bio value', () => {
-    function callback() {
-      expect(wrapper.vm.user.bio).toBe('bio test');
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.bio).toBe('bio test');
+  });
+  it('expects user has a email value', () => {
+    expect(wrapper.vm.user.email).toBe('email@test.com');
   });
   it('expects user has a followers value', () => {
-    function callback() {
-      expect(wrapper.vm.user.followers).toBe(999);
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.followers).toBe(999);
   });
   it('expects user has a following value', () => {
-    function callback() {
-      expect(wrapper.vm.user.following).toBe(999);
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.following).toBe(999);
   });
   it('expects user has a avatar_url value', () => {
-    function callback() {
-      expect(wrapper.vm.user.avatar_url).toBe('/sample.jpg');
-    }
-    actions.fetchUser(callback);
-  });
-  it('expects user has a avatar_url value', () => {
-    function callback() {
-      wrapper.vm.getUserState();
-      expect(wrapper.vm.user.avatar_url).toBe('/sample.jpg');
-    }
-    actions.fetchUser(callback);
+    expect(wrapper.vm.user.avatar_url).toBe('/sample.jpg');
   });
 });
